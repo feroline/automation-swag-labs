826afcdab096bea0276c29969366ac20521d4e20
Ana Carolina Rodrigues Rocha <anacarolina.rrocha@gmail.com>
2024-05-13 19:47:25 -0300

    refatorando login e adicionando specs

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -3,12 +3,14 @@ import URLS from "../support/elements/URLS.cy";
 import LoginElements from "../support/elements/LoginElements.cy";
 import HomeElements from "../support/elements/HomeElements.cy";
 
+beforeEach(() => {
+  cy.fixture("login").as("loginFixture")
+});
+
 describe("Login", () => {
   it("Login com usuário Standard", () => {
 
-    cy.fixture("login")
-      .as("loginFixture")
-      .then((usuario) => {
+    cy.get('@loginFixture').then((usuario) => {
         cy.inserirLogin(usuario.usernames.standard_user, usuario.password);
       });
       
@@ -18,43 +20,33 @@ describe("Login", () => {
 
   it("Login com usuário locked_out_user", () => {
 
-    const messageErro = "Epic sadface: Sorry, this user has been locked out."
-    
-    cy.fixture("login")
-    .as("loginFixture")
-    .then((usuario) => {
+    cy.get('@loginFixture').then((usuario) => {
       cy.inserirLogin(usuario.usernames.locked_out_user, usuario.password);
+      cy.get(LoginElements.divErrorMessage).should('contain', usuario.mensagensErro.errorMessagelockedOut);
     });
     
-    cy.get(LoginElements.divErrorMessage).should('contain', messageErro);
     cy.url().should("contains", URLS.LOGIN);
 
   
   });
 
   it("Login com problem_user", () => {
-    let imagemDeErro = "/static/media/sl-404.168b1cce.jpg"
 
-    cy.fixture("login")
-    .as("loginFixture")
-    .then((usuario) => {
+    cy.get('@loginFixture').then((usuario) => {
       cy.inserirLogin(usuario.usernames.problem_user, usuario.password);
+      
+      cy.url().should("contains", URLS.HOME);
+      
+      cy.get(HomeElements.imagemDeErro)
+      .should('have.attr', 'src', usuario.mensagensErro.imagemDeErro)
     });
-  
-    cy.url().should("contains", URLS.HOME);
-
-    cy.get(HomeElements.imagemDeErro)
-      .should('have.attr', 'src', imagemDeErro)
    
 
   });
 
   it("Login com performance_glitch_user", () => {
-    const messageErro = "Epic sadface: Sorry, this user has been locked out."
-    
-    cy.fixture("login")
-    .as("loginFixture")
-    .then((usuario) => {
+
+    cy.get('@loginFixture').then((usuario) => {
       cy.inserirLogin(usuario.usernames.performance_glitch_user, usuario.password);
     });
      
@@ -68,9 +60,8 @@ describe("Login", () => {
   
   
   it("Login com usuário error_user", () => {
-    cy.fixture("login")
-    .as("loginFixture")
-    .then((usuario) => {
+
+    cy.get('@loginFixture').then((usuario) => {
       cy.inserirLogin(usuario.usernames.error_user, usuario.password);
     });
 
@@ -95,9 +86,7 @@ describe("Login", () => {
 
   it("Login com usuário visual_user", () => {
 
-    cy.fixture("login")
-    .as("loginFixture")
-    .then((usuario) => {
+    cy.get('@loginFixture').then((usuario) => {
       cy.inserirLogin(usuario.usernames.visual_user, usuario.password);
     });
 
@@ -106,13 +95,12 @@ describe("Login", () => {
   });
 
   it("Login com usuário e senha inválidos", () => {
-    cy.fixture("login")
-    .as("loginFixture")
-    .then((usuario) => {
+    
+    cy.get('@loginFixture').then((usuario) => {
       cy.inserirLogin(usuario.usuarioInvalido.username, usuario.usuarioInvalido.password);
       
       cy.get(LoginElements.divErrorMessage)
-        .should('contain', usuario.usuarioInvalido.errorMessage);
+        .should('contain', usuario.mensagensErro.errorMessageGeneric);
     });
     
     cy.url().should("contains", URLS.LOGIN);
@@ -122,13 +110,11 @@ describe("Login", () => {
   
   it("Login com usuário inválido e senha válida", () => {
 
-    cy.fixture("login")
-    .as("loginFixture")
-    .then((usuario) => {
+    cy.get('@loginFixture').then((usuario) => {
       cy.inserirLogin(usuario.usuarioInvalido.password, usuario.password);
       
       cy.get(LoginElements.divErrorMessage)
-        .should('contain', usuario.usuarioInvalido.errorMessage);
+        .should('contain', usuario.mensagensErro.errorMessageGeneric);
     });
     
     cy.url().should("contains", URLS.LOGIN);
@@ -137,13 +123,11 @@ describe("Login", () => {
 
   it("Login com usuário válido e senha inválida", () => {
 
-    cy.fixture("login")
-    .as("loginFixture")
-    .then((usuario) => {
+    cy.get('@loginFixture').then((usuario) => {
       cy.inserirLogin(usuario.usernames.standard_user, usuario.usuarioInvalido.password);
       
       cy.get(LoginElements.divErrorMessage)
-        .should('contain', usuario.usuarioInvalido.errorMessage);
+        .should('contain', usuario.mensagensErro.errorMessageGeneric);
     });
     
     cy.url().should("contains", URLS.LOGIN);
@@ -152,13 +136,11 @@ describe("Login", () => {
 
   it("Login com usuário inválido e senha vazia", () => {
 
-    cy.fixture("login")
-    .as("loginFixture")
-    .then((usuario) => {
+    cy.get('@loginFixture').then((usuario) => {
       cy.inserirLogin(usuario.usernames.standard_user, usuario.usuarioSenhaVazio.password);
       
       cy.get(LoginElements.divErrorMessage)
-        .should('contain', usuario.usuarioInvalido.errorMessage);
+        .should('contain', usuario.mensagensErro.errorMessageGeneric);
     });
     
     cy.url().should("contains", URLS.LOGIN);
@@ -168,13 +150,11 @@ describe("Login", () => {
 
   it("Login com usuário inválido e senha vazia", () => {
 
-    cy.fixture("login")
-    .as("loginFixture")
-    .then((usuario) => {
+    cy.get('@loginFixture').then((usuario) => {
       cy.inserirLogin(usuario.usuarioSenhaVazio.username, usuario.password);
       
       cy.get(LoginElements.divErrorMessage)
-        .should('contain', usuario.usuarioInvalido.errorMessage);
+        .should('contain', usuario.mensagensErro.errorMessageGeneric);
     });
     
     cy.url().should("contains", URLS.LOGIN);
@@ -182,13 +162,12 @@ describe("Login", () => {
   });
 
   it("Login com usuário e senhas vazios", () => {
-    cy.fixture("login")
-    .as("loginFixture")
-    .then((usuario) => {
+    
+    cy.get('@loginFixture').then((usuario) => {
       cy.inserirLogin(usuario.usuarioSenhaVazio.username, usuario.usuarioSenhaVazio.password);
       
       cy.get(LoginElements.divErrorMessage)
-        .should('contain', usuario.usuarioInvalido.errorMessage);
+        .should('contain', usuario.mensagensErro.errorMessageGeneric);
     });
     
     cy.url().should("contains", URLS.LOGIN);
@@ -203,7 +182,7 @@ describe("Login", () => {
 
     cy.fixture("login").as("loginFixture").then((usuario) => {
       cy.get(LoginElements.divErrorMessage)
-        .should('contain', usuario.errorMessageUsername);
+        .should('contain', usuario.mensagensErro.errorMessageUsername);
     });
     
     cy.url().should("contains", URLS.LOGIN);
