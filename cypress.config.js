d83d040479c322d3a56429318c80f101adb9c024
Ana Carolina Rodrigues Rocha <anacarolina.rrocha@gmail.com>
2024-05-15 17:07:36 -0300

    adicionando cypress yml

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  
+  projectId: "9edqig",
   e2e: {
     chromeWebSecurity: false,
     baseUrl: "https://www.saucedemo.com",
