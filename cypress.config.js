b40f17753877051a68af44ff2dff248641b916bb
Ana Carolina Rodrigues Rocha <anacarolina.rrocha@gmail.com>
2024-05-15 16:58:05 -0300

    corrigindo chave

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "h2v9oq",
+  
   e2e: {
     chromeWebSecurity: false,
     baseUrl: "https://www.saucedemo.com",
